e9d2569ba74b094b5abc7cb4eee2df408f1afabe
Madhawakavindu <madhawakavindu7@gmail.com>
2025-12-18 21:12:50 +0530

    update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,10 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+//import 'package:cafeteria/main.dart';
+import 'package:cafeteria/app.dart';
 
-import 'package:cafeteria/main.dart';
+//import 'package:cafeteria/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
